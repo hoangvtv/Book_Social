b2cb466d273d06020178cf65af557943cde5748c
hoangvtv <phamtanhoangvtv@gmail.com>
2024-10-17 23:48:42 +0700

    feat: handle isolation unit test with h2 database

--- identity_service/src/test/java/com/phamtanhoang/identity_service/service/UserServiceTest.java
+++ identity_service/src/test/java/com/phamtanhoang/identity_service/service/UserServiceTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
 
@@ -23,6 +24,7 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource("/test.properties")
 public class UserServiceTest {
 
   @Autowired
