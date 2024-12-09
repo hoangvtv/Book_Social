ee132e9224a6c0753ca3bf4cb9d84cc38d5baeae
hoangpt <hoangpt@splus-software.com.vn>
2024-12-09 15:23:16 +0700

    fet: handle authorization with keycloak

--- profile-service/src/main/java/com/phamtanhoang/profile/configuration/SecurityConfig.java
+++ profile-service/src/main/java/com/phamtanhoang/profile/configuration/SecurityConfig.java
@@ -3,13 +3,17 @@ package com.phamtanhoang.profile.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
   private final String[] PUBLIC_ENDPOINTS = {"/register"};
 
@@ -20,11 +24,20 @@ public class SecurityConfig {
         .anyRequest()
         .authenticated());
 
-    httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
+    httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(
+            jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter()))
         .authenticationEntryPoint(new JwtAuthenticationEntryPoint()));
 
     httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
     return httpSecurity.build();
+  }
+
+  @Bean
+  public JwtAuthenticationConverter jwtAuthenticationConverter() {
+    JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+    converter.setJwtGrantedAuthoritiesConverter(new CustomAuthoritiesConverter());
+
+    return converter;
   }
 }
