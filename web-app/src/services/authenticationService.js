5676c945312cb73a551d5b1499524576b9251367
hoangvtv <phamtanhoangvtv@gmail.com>
2024-12-27 01:00:35 +0700

    feat: init react app

---
+++ web-app/src/services/authenticationService.js
@@ -0,0 +1,26 @@
+import { getToken, removeToken, setToken } from "./localStorageService";
+import httpClient from "../configurations/httpClient";
+import { API } from "../configurations/configuration";
+
+export const logIn = async (username, password) => {
+
+  console.log("username", username);
+  console.log("password", password);
+  
+  const response = await httpClient.post(API.LOGIN, {
+    username: username,
+    password: password,
+  });
+
+  setToken(response.data?.result?.token);
+
+  return response;
+};
+
+export const logOut = () => {
+  removeToken();
+};
+
+export const isAuthenticated = () => {
+  return getToken();
+};
