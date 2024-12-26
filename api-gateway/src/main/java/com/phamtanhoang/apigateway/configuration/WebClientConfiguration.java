59e863f41b420ae6bda97311bdbb51971b932f8d
hoangvtv <phamtanhoangvtv@gmail.com>
2024-12-27 00:28:30 +0700

    feat: handle config cors for api-gateway

--- api-gateway/src/main/java/com/phamtanhoang/apigateway/configuration/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/phamtanhoang/apigateway/configuration/WebClientConfiguration.java
@@ -4,12 +4,30 @@ package com.phamtanhoang.apigateway.configuration;
 import com.phamtanhoang.apigateway.repository.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+import java.util.List;
+
 @Configuration
 public class WebClientConfiguration {
+
+  @Bean
+  CorsWebFilter corsWebFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("*"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowedMethods(List.of("*"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsWebFilter(source);
+  }
 
   @Bean
   WebClient webClient(){
