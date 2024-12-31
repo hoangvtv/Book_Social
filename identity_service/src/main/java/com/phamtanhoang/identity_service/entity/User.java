24d6df270bbcaa7442a40895d5f8c4913fe0fe61
hoangvtv <phamtanhoangvtv@gmail.com>
2024-12-31 23:23:15 +0700

    feat: handle send mail with kafka

--- identity_service/src/main/java/com/phamtanhoang/identity_service/entity/User.java
+++ identity_service/src/main/java/com/phamtanhoang/identity_service/entity/User.java
@@ -21,6 +21,9 @@ public class User {
     @Column(name = "username", unique = true, columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci")
     String username;
     String password;
+    @Column(name = "email", unique = true, columnDefinition = "VARCHAR(255) COLLATE utf8mb4_unicode_ci")
+    String email;
+    @Column(name = "email_verified", nullable = false, columnDefinition = "boolean default false")
     @ManyToMany
     Set<Role> roles;
 }
