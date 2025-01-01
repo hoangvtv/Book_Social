60cfbb32e64328eb6128cc07be7d80fcfd084c65
hoangvtv <phamtanhoangvtv@gmail.com>
2025-01-01 23:58:57 +0700

    feat: apply map-base-strategy in get created of post

--- post-service/src/main/java/com/phamtanhoang/postservice/dto/response/PostResponse.java
+++ post-service/src/main/java/com/phamtanhoang/postservice/dto/response/PostResponse.java
@@ -18,6 +18,7 @@ public class PostResponse {
     String id;
     String content;
     String userId;
+    String createdAt;
     Instant createdDate;
     Instant modifiedDate;
 }
---
+++ post-service/src/main/java/com/phamtanhoang/postservice/service/DateTimeFormatter.java
@@ -0,0 +1,61 @@
+package com.phamtanhoang.postservice.service;
+
+
+import com.phamtanhoang.postservice.exception.AppException;
+import com.phamtanhoang.postservice.exception.ErrorCode;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class DateTimeFormatter {
+
+  Map<Long, Function<Instant, String>> strategyMap = new LinkedHashMap<>();
+
+  public DateTimeFormatter() {
+    strategyMap.put(60L, this::formatInSeconds);
+    strategyMap.put(3600L, this::formatInMinutes);
+    strategyMap.put(86400L, this::formatInHours);
+    strategyMap.put(Long.MAX_VALUE, this::formatInDate);
+  }
+
+  public String formatDate(Instant instant) {
+    long elapseSeconds = ChronoUnit.SECONDS.between(instant, Instant.now());
+
+    var strategy = strategyMap.entrySet()
+        .stream()
+        .filter(longFunctionEntry -> elapseSeconds < longFunctionEntry.getKey())
+        .findFirst().orElseThrow(() -> new AppException(ErrorCode.STRATEGY_NOT_FOUND));
+
+    return strategy.getValue().apply(instant);
+  }
+
+  private String formatInSeconds(Instant instant){
+    long elapseSeconds = ChronoUnit.SECONDS.between(instant, Instant.now());
+    return String.format("%d seconds ago", elapseSeconds);
+  }
+
+  private String formatInMinutes(Instant instant){
+    long elapseMinutes = ChronoUnit.MINUTES.between(instant, Instant.now());
+    return String.format("%d minutes ago", elapseMinutes);
+  }
+
+  private String formatInHours(Instant instant){
+    long elapseHours = ChronoUnit.HOURS.between(instant, Instant.now());
+    return String.format("%d hours ago", elapseHours);
+  }
+
+  private String formatInDate(Instant instant){
+    LocalDateTime localDateTime = instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
+    java.time.format.DateTimeFormatter formatter =
+        java.time.format.DateTimeFormatter.ISO_DATE;
+    return localDateTime.format(formatter);
+  }
+
+}
