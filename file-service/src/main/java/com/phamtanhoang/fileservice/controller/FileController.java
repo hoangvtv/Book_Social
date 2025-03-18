af4504495e19e2d449e59f09323a7bb188d068ce
hoangvtv <phamtanhoangvtv@gmail.com>
2025-03-18 22:23:28 +0700

    feat: handle upload file service

---
+++ file-service/src/main/java/com/phamtanhoang/fileservice/controller/FileController.java
@@ -0,0 +1,29 @@
+package com.phamtanhoang.fileservice.controller;
+
+import com.phamtanhoang.fileservice.dto.ApiResponse;
+import com.phamtanhoang.fileservice.service.FileService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class FileController {
+
+  FileService fileService;
+
+  @PostMapping("media/upload")
+  public ApiResponse<Object> upload(@RequestParam("file") MultipartFile file) throws IOException {
+
+    return ApiResponse.builder()
+        .result(fileService.uploadFile(file))
+        .build();
+  }
+}
